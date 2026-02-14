50d814f07f7fa9a8df775f5041170a3cced3bdd7
Raimison <raimison.ti@gmail.com>
2026-02-13 22:38:47 -0400

    Tipos Explícitos, Array e tuplas.

--- tipos/tipos.js
+++ tipos/tipos.js
@@ -2,12 +2,29 @@
 // string
 let nome = 'Rafaela';
 console.log(nome);
-// nome = 28
+// nome = 28 erro
 // numbers
 let idade = 28;
-// idade = 'Daiana'
+// idade = 'Rafaela Daiana'
 idade = 28.5;
 console.log(idade);
+// boolean
 let possuiHobbies = false;
 // possuiHobbies = 1
 console.log(possuiHobbies);
+// tipos explícitos
+let minhaIdade;
+minhaIdade = 28;
+console.log(typeof minhaIdade);
+// minhaIdade = '28' gera erro
+// array
+let hobbies = ["cozinha", "Praticas Esportes"];
+console.log(hobbies[0]);
+console.log(typeof hobbies);
+hobbies = [100];
+// hobbies = 100 erro
+console.log(hobbies);
+// tuplas
+let endereco = ["Av Principal", 99, ""];
+console.log(endereco);
+endereco = ["Rua Importante", 1260, "Bloco A"];
--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -1,14 +1,36 @@
 // string
-let nome = 'Rafaela'
+let nome: string = 'Rafaela'
 console.log(nome)
-// nome = 28
+// nome = 28 erro
 
 // numbers
-let idade = 28
-// idade = 'Daiana'
+let idade: number = 28
+// idade = 'Rafaela Daiana'
 idade = 28.5
 console.log(idade);
 
-let possuiHobbies = false
+// boolean
+let possuiHobbies: boolean = false
 // possuiHobbies = 1
 console.log(possuiHobbies);
+
+// tipos explícitos
+let minhaIdade: number
+minhaIdade = 28
+console.log(typeof minhaIdade)
+// minhaIdade = '28' gera erro
+
+// array
+let hobbies: any[] = ["cozinha", "Praticas Esportes"]
+console.log(hobbies[0])
+console.log(typeof hobbies)
+
+hobbies = [100]
+// hobbies = 100 erro
+console.log(hobbies)
+
+// tuplas
+let endereco: [string, number, string] = ["Av Principal", 99, ""]
+console.log(endereco)
+
+endereco = ["Rua Importante", 1260, "Bloco A"]
