a9592a7017363c98acc9bceeeef98013a3be3c71
Raimison <raimison.ti@gmail.com>
2026-02-23 22:29:57 -0400

    Finalizada aula 46.

---
+++ ecmascript/ecmascript.js
@@ -0,0 +1,12 @@
+"use strict";
+// let & const
+let seraQuePode = '?'; // hoisting somente no var
+console.log(seraQuePode);
+let estaFrio = true;
+if (estaFrio) {
+    let acao = 'Colcoar o casaco!';
+    console.log(acao);
+}
+const cpf = '123.456.000-99';
+// cpf = '789.101.132-78'
+console.log(cpf);
---
+++ ecmascript/ecmascript.ts
@@ -0,0 +1,13 @@
+// let & const
+let seraQuePode = '?'// hoisting somente no var
+console.log(seraQuePode)
+
+let estaFrio = true
+if (estaFrio) {
+  let acao = 'Colcoar o casaco!'
+  console.log(acao)
+}
+
+const cpf: string = '123.456.000-99'
+// cpf = '789.101.132-78'
+console.log(cpf)
